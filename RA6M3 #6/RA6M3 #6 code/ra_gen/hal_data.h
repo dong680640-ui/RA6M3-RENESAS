03843696c4df591d481b2b77c9fa7b6c2436088c
dong680640-ui <dong680640@gmail.com>
2025-08-26 18:35:45 +0900

    Add files via upload

---
+++ RA6M3 #6/RA6M3 #6 code/ra_gen/hal_data.h
@@ -0,0 +1,48 @@
+/* generated HAL header file - do not edit */
+#ifndef HAL_DATA_H_
+#define HAL_DATA_H_
+#include <stdint.h>
+#include "bsp_api.h"
+#include "common_data.h"
+#include "r_agt.h"
+#include "r_timer_api.h"
+#include "r_icu.h"
+#include "r_external_irq_api.h"
+#include "r_sci_uart.h"
+#include "r_uart_api.h"
+FSP_HEADER
+/** AGT Timer Instance */
+extern const timer_instance_t g_timer0;
+
+/** Access the AGT instance using these structures when calling API functions directly (::p_api is not used). */
+extern agt_instance_ctrl_t g_timer0_ctrl;
+extern const timer_cfg_t g_timer0_cfg;
+
+#ifndef R_AGT0_Interrupt
+void R_AGT0_Interrupt(timer_callback_args_t *p_args);
+#endif
+/** External IRQ on ICU Instance. */
+extern const external_irq_instance_t g_external_irq11;
+
+/** Access the ICU instance using these structures when calling API functions directly (::p_api is not used). */
+extern icu_instance_ctrl_t g_external_irq11_ctrl;
+extern const external_irq_cfg_t g_external_irq11_cfg;
+
+#ifndef R_IRQ11_Interrupt
+void R_IRQ11_Interrupt(external_irq_callback_args_t *p_args);
+#endif
+/** UART on SCI Instance. */
+extern const uart_instance_t g_uart0;
+
+/** Access the UART instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_uart_instance_ctrl_t g_uart0_ctrl;
+extern const uart_cfg_t g_uart0_cfg;
+extern const sci_uart_extended_cfg_t g_uart0_cfg_extend;
+
+#ifndef user_uart_callback
+void user_uart_callback(uart_callback_args_t *p_args);
+#endif
+void hal_entry(void);
+void g_hal_init(void);
+FSP_FOOTER
+#endif /* HAL_DATA_H_ */
